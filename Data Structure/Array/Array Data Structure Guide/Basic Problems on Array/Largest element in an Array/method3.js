b8c084ca8d7928f194d746a3baf6800e34c93e23
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-19 13:48:07 +0530

    Built in Approach to solve this problem is to traverse the whole array and find the maximum among them in javascript

---
+++ Data Structure/Array/Array Data Structure Guide/Basic Problems on Array/Largest element in an Array/method3.js
@@ -0,0 +1,8 @@
+// Function to find the largest number in an array
+function largest(arr) {
+    return Math.max(...arr);
+}
+
+// Driver Code
+const arr = [10, 324, 45, 90, 9808];
+console.log(largest(arr));
